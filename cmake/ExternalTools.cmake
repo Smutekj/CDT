3bcf8ef68fcf2a7076458f9f55defc971715c09b
J S <smutek.jakub@seznam.cz>
2024-06-25 20:25:34 +0200

    added options for build with example and tests

--- cmake/ExternalTools.cmake
+++ cmake/ExternalTools.cmake
@@ -1,51 +1,55 @@
 include(FetchContent)
 
+if(BUILD_EXAMPLE)
+  ### SFML ###
+  FetchContent_Declare(
+    sfml
+    URL https://github.com/SFML/SFML/archive/refs/tags/2.5.1.zip
+    URL_MD5 2c4438b3e5b2d81a6e626ecf72bf75be
+  )
+  set(BUILD_SHARED_LIBS OFF)
+  set(SFML_BUILD_EXAMPLES OFF)
+  set(SFML_BUILD_DOC OFF)
+  set(SFML_BUILD_NETWORK OFF)
+  set(SFML_BUILD_AUDIO OFF)
+  set(SFML_BUILD_GRAPHICS ON)
+  set(SFML_BUILD_WINDOW ON)
 
-### SFML ###
-FetchContent_Declare(
-  sfml
-  URL https://github.com/SFML/SFML/archive/refs/tags/2.5.1.zip
-  URL_MD5 2c4438b3e5b2d81a6e626ecf72bf75be
-)
-set(BUILD_SHARED_LIBS OFF)
-set(SFML_BUILD_EXAMPLES OFF)
-set(SFML_BUILD_DOC OFF)
-set(SFML_BUILD_NETWORK OFF)
-set(SFML_BUILD_AUDIO OFF)
-set(SFML_BUILD_GRAPHICS ON)
-set(SFML_BUILD_WINDOW ON)
+  FetchContent_GetProperties(sfml)
+  if(NOT sfml_POPULATED)
+    FetchContent_Populate(sfml)
+    add_subdirectory(${sfml_SOURCE_DIR})
+  endif()
 
-FetchContent_GetProperties(sfml)
-if(NOT sfml_POPULATED)
-  FetchContent_Populate(sfml)
-  add_subdirectory(${sfml_SOURCE_DIR})
+  # Dear ImGui
+  FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui
+    GIT_TAG 35b1148efb839381b84de9290d9caf0b66ad7d03 # 1.82
+  )
+
+  FetchContent_MakeAvailable(imgui)
+
+  # ImGui-SFML
+  FetchContent_Declare(
+    imgui-sfml
+    GIT_REPOSITORY https://github.com/SFML/imgui-sfml
+    GIT_TAG 82dc2033e51b8323857c3ae1cf1f458b3a933c35 # 2.3
+  )
+  set(IMGUI_DIR ${imgui_SOURCE_DIR})
+  set(IMGUI_SFML_FIND_SFML OFF)
+  set(IMGUI_SFML_IMGUI_DEMO ON)
+
+  FetchContent_MakeAvailable(imgui-sfml)
 endif()
 
-# Dear ImGui
-FetchContent_Declare(
-  imgui
-  GIT_REPOSITORY https://github.com/ocornut/imgui
-  GIT_TAG 35b1148efb839381b84de9290d9caf0b66ad7d03 # 1.82
-)
 
-FetchContent_MakeAvailable(imgui)
-
-# ImGui-SFML
-FetchContent_Declare(
-  imgui-sfml
-  GIT_REPOSITORY https://github.com/SFML/imgui-sfml
-  GIT_TAG 82dc2033e51b8323857c3ae1cf1f458b3a933c35 # 2.3
-)
-set(IMGUI_DIR ${imgui_SOURCE_DIR})
-set(IMGUI_SFML_FIND_SFML OFF)
-set(IMGUI_SFML_IMGUI_DEMO ON)
-
-FetchContent_MakeAvailable(imgui-sfml)
-
-# Google Test
-FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        main
-)
-FetchContent_MakeAvailable(googletest)
+if(BUILD_TESTS)
+  # Google Test
+  FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG        main
+  )
+  FetchContent_MakeAvailable(googletest)
+endif()
