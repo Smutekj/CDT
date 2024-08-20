b02d5157362c6a608a2e8143e284a5dfd83fe13a
J S <smutek.jakub@seznam.cz>
2024-08-20 13:54:38 +0200

    removed -march compiler flag

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,7 +1,7 @@
 add_compile_options($<$<CONFIG:Debug>:-O0>)
 
 # in Release mode, add aggressive optimizations
-add_compile_options($<$<CONFIG:Release>:-Ofast>)
+add_compile_options($<$<CONFIG:Release>:-O2>)
 add_compile_options($<$<CONFIG:Release>:-fno-finite-math-only>)
 # add_compile_options($<$<CONFIG:Release>:-fopenmp>)
 # add_compile_options($<$<CONFIG:Release>:-Rpass-analysis=loop-vectorize>)
@@ -9,7 +9,7 @@ add_compile_options($<$<CONFIG:Release>:-fno-finite-math-only>)
 
 
 if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
-    add_compile_options($<$<CONFIG:Release>:-march=native>)
+    # add_compile_options($<$<CONFIG:Release>:-march=native>)
 endif()
 
 option(ENABLE_APPROXMATH "Use approximate math" off) 
