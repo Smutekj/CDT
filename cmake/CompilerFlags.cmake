6351c173745c22b9aa5abeb027d5fb91e5c83820
Smutekj <smutek.jakub@seznam.cz>
2024-12-02 04:29:32 +0100

    Update CompilerFlags.cmake

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -4,9 +4,9 @@ add_compile_options($<$<CONFIG:Release>:-O2>)
 add_compile_options($<$<CONFIG:Release>:-fno-finite-math-only>)
 
 
-if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
+# if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
     # add_compile_options($<$<CONFIG:Release>:-march=native>)
-endif()
+# endif()
 
 option(ENABLE_APPROXMATH "Use approximate math" off) 
 if (ENABLE_APPROXMATH)
