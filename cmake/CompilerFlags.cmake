2fe54094003dc137224b770e6ed94c148fd72515
Smutekj <smutek.jakub@seznam.cz>
2025-01-18 18:37:02 +0100

    made compiler flags private

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,14 +1,17 @@
 
-if(MSVC)
-add_compile_options($<$<CONFIG:Debug>:/Od>)
-# in Release mode, add aggressive optimizations
-add_compile_options($<$<CONFIG:Release>:/O2>)
-else()
-add_compile_options($<$<CONFIG:Debug>:-O0>)
-# in Release mode, add aggressive optimizations
-add_compile_options($<$<CONFIG:Release>:-O2>)
-endif()
+function(set_compiler_flags project_name)
+    if(MSVC)
+    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Debug>:/Od>)
+    # in Release mode, add aggressive optimizations
+    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:/O2>)
+    else()
+    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Debug>:-O0>)
+    # in Release mode, add aggressive optimizations
+    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:-O2>)
+    endif()
 
-if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
-    add_compile_options($<$<CONFIG:Release>:-march=native>)
-endif()
+    if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
+    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:-march=native>)
+    endif()
+
+endfunction()
