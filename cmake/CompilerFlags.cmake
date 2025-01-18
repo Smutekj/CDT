00ec1a8b9606e90e9b38572f6c4d62ed0533f194
Smutekj <smutek.jakub@seznam.cz>
2025-01-18 18:22:50 +0100

    cleaned cmake

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,14 +1,14 @@
+
+if(MSVC)
+add_compile_options($<$<CONFIG:Debug>:/Od>)
+# in Release mode, add aggressive optimizations
+add_compile_options($<$<CONFIG:Release>:/O2>)
+else()
 add_compile_options($<$<CONFIG:Debug>:-O0>)
 # in Release mode, add aggressive optimizations
 add_compile_options($<$<CONFIG:Release>:-O2>)
-add_compile_options($<$<CONFIG:Release>:-fno-finite-math-only>)
+endif()
 
-
-# if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
-    # add_compile_options($<$<CONFIG:Release>:-march=native>)
-# endif()
-
-option(ENABLE_APPROXMATH "Use approximate math" off) 
-if (ENABLE_APPROXMATH)
-    add_definitions(-DFAU_APPROXMATH)
-endif ()
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
+    add_compile_options($<$<CONFIG:Release>:-march=native>)
+endif()
