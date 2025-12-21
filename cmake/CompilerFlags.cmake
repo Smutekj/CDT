947ece475f814373feb623134176e0654e2074a4
J S <smutek.jakub@seznam.cz>
2025-12-21 16:36:51 +0100

    -march option only when not Crosscompiling

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,17 +1,17 @@
 
 function(set_compiler_flags project_name)
     if(MSVC)
-    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Debug>:/Od>)
-    # in Release mode, add aggressive optimizations
-    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:/O2>)
+        target_compile_options(${project_name} PRIVATE $<$<CONFIG:Debug>:/Od>)
+        # in Release mode, add aggressive optimizations
+        target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:/O2>)
     else()
-    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Debug>:-O0>)
-    # in Release mode, add aggressive optimizations
-    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:-O2>)
+        target_compile_options(${project_name} PRIVATE $<$<CONFIG:Debug>:-O0>)
+        # in Release mode, add aggressive optimizations
+        target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:-O2>)
     endif()
 
-    if (CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
-    target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:-march=native>)
+    if (NOT CMAKE_CROSSCOMPILING AND CMAKE_SYSTEM_PROCESSOR MATCHES "(x86)|(X86)|(amd64)|(AMD64)")
+        target_compile_options(${project_name} PRIVATE $<$<CONFIG:Release>:-march=native>)
     endif()
 
 endfunction()
